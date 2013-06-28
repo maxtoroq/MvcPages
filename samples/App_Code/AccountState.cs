d6fb42b3d8e3a322511140d30bd90082457739d9
Max Toro <maxtoroq@gmail.com>
2013-06-28 17:18:31 -0400

    samples changes and nuget packages

---
+++ samples/App_Code/AccountState.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class AccountState {
+   
+   public static readonly string UserName = "demo";
+   public static string Password = "demo";
+}
